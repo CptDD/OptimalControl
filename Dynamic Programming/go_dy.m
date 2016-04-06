b047f0c4547139496db5386770ca174b1f18e247
CptDD <adydani_mezei@yahoo.com>
2016-04-06 22:47:33 +0300

    Added computation of optimal states

--- Dynamic Programming/go_dy.m
+++ Dynamic Programming/go_dy.m
@@ -3,6 +3,11 @@ N=3;
 xk=[0 1/2 1 3/2];
 uk=[-1 -1/2 0 1/2 1];
 
+state_index=3;
+current_state=xk(state_index);
+
+final_states=[];
+
 %computing the J vector for the initial values
 JNN=compute_J(N,N,xk,uk);
 JNN_A(:,N)=JNN';
@@ -25,3 +30,18 @@ for k=N-1:-1:1
    Uoptim_A(:,k)=Uoptim_temp;
    JNN_A(:,k)=JNN_temp';
 end
+
+final_states=[final_states current_state];
+for k=1:N-1
+    u_optimal(k)=Uoptim_A(state_index,k);
+    current_state=current_state+u_optimal(k);
+    state_index=find(xk==current_state,1);
+    final_states=[final_states current_state];
+end
+
+subplot(2,1,1)
+plot(final_states)
+grid
+subplot(2,1,2)
+plot(u_optimal)
+grid
